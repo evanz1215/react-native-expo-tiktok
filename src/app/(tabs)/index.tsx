626389b48bb5cac36331bfae725855eae08b4091
Evan <evanz1215@gmail.com>
2025-11-18 22:56:56 +0800

    新增 FeedTab 組件，更新 HomeScreen 組件以支持選項卡導航

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,9 +1,24 @@
 import PostListItem from "@/components/PostListItem";
-import { View, FlatList, Dimensions, ViewToken, Text } from "react-native";
+import {
+  View,
+  FlatList,
+  Dimensions,
+  ViewToken,
+  Text,
+  StyleSheet,
+} from "react-native";
 import posts from "@assets/data/posts.json";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { useBottomTabBarHeight } from "@react-navigation/bottom-tabs";
 import { useRef, useState } from "react";
+import { MaterialIcons, Ionicons } from "@expo/vector-icons";
+import FeedTab from "@/components/GenericComponents/FeedTab";
+
+const TABS = {
+  EXPLORER: "Explorer",
+  FOLLOWING: "Following",
+  FOR_YOU: "For You",
+} as const;
 
 export default function HomeScreen() {
   const { height } = Dimensions.get("window");
@@ -11,6 +26,7 @@ export default function HomeScreen() {
   const tabBarHeight = useBottomTabBarHeight();
   const containerHeight = height - tabBarHeight - (insets.bottom || 0);
   const [currentIndex, setCurrentIndex] = useState(0);
+  const [activeTab, setActiveTab] = useState(TABS.FOR_YOU);
   const onViewableItemChanged = useRef(
     ({ viewableItems }: { viewableItems: ViewToken[] }) => {
       if (viewableItems.length > 0) {
@@ -24,6 +40,28 @@ export default function HomeScreen() {
 
   return (
     <View style={{ height: containerHeight }}>
+      <View style={styles.topBar}>
+        <MaterialIcons name="live-tv" size={24} color="white" />
+        <View style={styles.navigationBar}>
+          <FeedTab
+            title={TABS.EXPLORER}
+            setActiveTab={setActiveTab}
+            activeTab={activeTab}
+          />
+          <FeedTab
+            title={TABS.FOLLOWING}
+            setActiveTab={setActiveTab}
+            activeTab={activeTab}
+          />
+          <FeedTab
+            title={TABS.FOR_YOU}
+            setActiveTab={setActiveTab}
+            activeTab={activeTab}
+          />
+        </View>
+        <Ionicons name="search" size={24} color="white" />
+      </View>
+
       <FlatList
         data={posts}
         renderItem={({ item, index }) => (
@@ -42,3 +80,19 @@ export default function HomeScreen() {
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  topBar: {
+    flexDirection: "row",
+    position: "absolute",
+    top: 70,
+    zIndex: 1,
+    paddingHorizontal: 15,
+  },
+  navigationBar: {
+    flexDirection: "row",
+    flex: 1,
+    justifyContent: "center",
+    gap: 30,
+  },
+});
