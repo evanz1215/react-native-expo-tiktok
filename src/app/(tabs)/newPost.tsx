9cf0134932a11391527c18342a625e5e5494915b
Evan <evanz1215@gmail.com>
2025-11-29 22:42:46 +0800

    新增相機功能，更新 NewPostScreen 組件以請求相機權限並顯示相機視圖

--- src/app/(tabs)/newPost.tsx
+++ src/app/(tabs)/newPost.tsx
@@ -1,9 +1,64 @@
-import { View, Text } from "react-native";
+import { CameraView, CameraType, useCameraPermissions } from "expo-camera";
+import { useEffect, useState } from "react";
+import { View, Text, Button, StyleSheet } from "react-native";
+import * as Linking from "expo-linking";
+import { MaterialIcons, Ionicons } from "@expo/vector-icons";
+import { router } from "expo-router";
 
 export default function NewPostScreen() {
+  const [facing, setFacing] = useState<CameraType>("back");
+  const [permission, requestPermission] = useCameraPermissions();
+
+  useEffect(() => {
+    if (permission && !permission.granted && permission.canAskAgain) {
+      requestPermission();
+    }
+  }, [permission]);
+
+  if (permission && !permission.granted && !permission.canAskAgain) {
+    return (
+      <View style={styles.permissionContainer}>
+        <Text style={styles.permissionText}>
+          We need your permission to use the camera
+        </Text>
+        <Button
+          title="Grant Permission"
+          onPress={() => Linking.openSettings()}
+        />
+      </View>
+    );
+  }
+
   return (
     <View>
-      <Text>Hello, Friends!</Text>
+      <CameraView style={styles.camera} facing={facing} />
+      <View>
+        <Ionicons
+          name="close"
+          size={40}
+          color="white"
+          onPress={() => {
+            router.back();
+          }}
+        />
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  permissionContainer: {
+    flex: 1,
+    justifyContent: "center",
+    padding: 20,
+  },
+  permissionText: {
+    color: "#fff",
+    textAlign: "center",
+    fontSize: 20,
+    fontWeight: "700",
+  },
+  camera: {
+    flex: 1,
+  },
+});
