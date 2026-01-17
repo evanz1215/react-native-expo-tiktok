bf65251f83dbf3bda92d5761fe4aaf0103f42666
Evan <evanz1215@gmail.com>
2026-01-18 00:07:09 +0800

    新增上傳影片至儲存空間的功能，並優化 fetchPosts 函數的錯誤處理

--- src/services/posts.ts
+++ src/services/posts.ts
@@ -1,11 +1,51 @@
 import { supabase } from "@/lib/supabase";
+import { PostInput } from "@/types/types";
+
+type StorageInput = {
+  fileName: string;
+  fileExtension: string;
+  fileBuffer: ArrayBuffer;
+};
 
 export const fetchPosts = async () => {
-  const { data, error } = await supabase
+  const { data } = await supabase
     .from("posts")
     .select("*, user:profiles(*), nrOfComments:comments(count)")
     .order("created_at", { ascending: true })
     .throwOnError();
+
+  return data;
+};
+
+export const uploadVideoToStorage = async (storageProps: StorageInput) => {
+  const { fileName, fileExtension, fileBuffer } = storageProps;
+
+  const { data, error } = await supabase.storage
+    .from("videos")
+    .upload(fileName, fileBuffer, {
+      contentType: `video/${fileExtension}`,
+    });
+
+  if (error) {
+    throw error;
+  }
+
+  const { data: urlData } = supabase.storage
+    .from("videos")
+    .getPublicUrl(fileName);
+
+  return urlData.publicUrl;
+};
+
+export const createPost = async (newPosts: PostInput) => {
+  const { data, error } = await supabase
+    .from("posts")
+    .insert(newPosts)
+    .throwOnError();
+
+  if (error) {
+    throw error;
+  }
 
   return data;
 };
